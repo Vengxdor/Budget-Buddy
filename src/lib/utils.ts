d6507be7fc7d998d40763386561b632b117593b9
Steven Fernandez <fernandezsteven185@gmail.com>
2024-11-18 15:37:14 -0600

    feat: Welcome message

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -11,11 +11,26 @@ export const todaysDate = () => {
   return date.toISOString().split('T')[0]
 }
 
-
 export const formatedDate = (dateString: string) => {
   const date = new Date(dateString)
 
-  const options: Intl.DateTimeFormatOptions = { year: 'numeric', month: 'short', day: '2-digit' }
+  const options: Intl.DateTimeFormatOptions = {
+    year: 'numeric',
+    month: 'short',
+    day: '2-digit',
+  }
 
   return date.toLocaleDateString('en-US', options)
+}
+
+export const getPartOfDay = () => {
+  const date = new Date()
+
+  const hour = date.getHours()
+
+  if (hour >= 6 && hour < 12) return 'Good morning'
+
+  if (hour >= 12 && hour < 24) return 'Good afternoon'
+
+  return 'Good night'
 }
